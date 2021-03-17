8dc392b8b6127b2b5f1f2551fe8e168a28d9a2e8
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2021-03-17 22:09:30 +0700

    make simple bot

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use BotMan\BotMan\BotMan;
 use Illuminate\Http\Request;
+use BotMan\BotMan\Messages\Incoming\Answer;
 use App\Conversations\ExampleConversation;
 
 class BotManController extends Controller
@@ -15,6 +16,16 @@ class BotManController extends Controller
     {
         $botman = app('botman');
 
+        $botman->hears('{message}', function($botman, $message) {
+            if ($message == 'hi') {
+                $this->askName($botman);
+            } else if ($message == 'joke') {
+                $this->startConversation($botman);
+            } else {
+                $botman->reply("write 'hi' for testing...");
+            }
+        });
+
         $botman->listen();
     }
 
@@ -24,6 +35,19 @@ class BotManController extends Controller
     public function tinker()
     {
         return view('tinker');
+    }
+
+    /**
+     * Place your BotMan logic here.
+     */
+    public function askName($botman)
+    {
+        $botman->ask('Hello! What is your Name?', function(Answer $answer) {
+
+            $name = $answer->getText();
+
+            $this->say('Nice to meet you '.$name);
+        });
     }
 
     /**
